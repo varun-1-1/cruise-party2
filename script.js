cba8c5869470d6d66e8f1771baf60b70d4026650
varun-1-1 <varun.nagarajan.11@gmail.com>
2026-03-01 22:34:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -28,14 +28,7 @@ const CHARACTERS = {
     name: "Akash",
     role: "Theo Whitaker — The Writer",
     motive: "You want more money, but you don’t kill anyone over it.",
-    traits: "Quiet, Clumsy, Parents died when you were young, so you're close with your older sister 
-Hate being in front of the camera 
-Drive a hybrid car 
-Cat owner, which you like to take funny pictures of your cat 
-The only job you ever had was writing 
-Chess is your game 
-Don't drink alcohol 
-",
+    traits: "Quiet, Clumsy, Parents died when you were young, so you're close with your older sister ",
     doThis: "[e.g., bring up the final battle scene; ask people about rewrites; steer convos toward contracts]",
     feels: "[e.g., you suspect X is lying; you admire Y; you think Z is incompetent]",
     outfit: "[optional: writer’s scarf, notebook, pen, fake script pages]",
